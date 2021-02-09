978e6ee07c32861108c6e19e5284c8c0f1ebb0af
Sean Kozen <sgkozen@gmail.com>
2021-02-09 12:26:37 +0900

    removed else

--- javascript/mathApp.js
+++ javascript/mathApp.js
@@ -59,9 +59,7 @@ function generateProblem () {
         } else {
             generateProblem();
         }
-    } else {
-        alert("Error!!!");
-    }  
+    }
     
     SetFocus();    
     
